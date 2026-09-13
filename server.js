b752a8111e9a50d25512b1b65c6b8f6d4c7983d4
Nikhil Pandey <116251187+pnikhil08@users.noreply.github.com>
2026-09-13 22:46:50 +0530

    Uncomment server initialization in server.js
    Uncomment server initialization code to start the server.

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-
-// import app from "./src/app.js"
-// import connectDB from "./src/config/database.js"
-
-// connectDB()
-// app.listen(3000, ()=>{
-//     console.log("Server is running on port 3000")
-// }) 
-
-
 import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
 

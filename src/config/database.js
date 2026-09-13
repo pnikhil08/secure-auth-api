4c61fba769f1699cddfc17699dd9a4f985338050
Nikhil Pandey <116251187+pnikhil08@users.noreply.github.com>
2026-09-13 22:47:40 +0530

    Clean up database.js by removing comments
    Removed commented-out code for database connection.

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,3 @@
-// // we use this file for connect database 
-// import mongoose from "mongoose";
-// import config from "./config.js";
-// async function connectDB() {
-    
-//     await mongoose.connect(config.MONGO_URI)
-
-//     console.log("Connectedd to DB")
-// }
-
-// export default connectDB;
-
-
-// Database connection configuration
-// Database connection configuration
-
 import mongoose from "mongoose";
 import config from "./config.js";
 
